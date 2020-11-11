cf0df5d712269cb1fd67fea9a9d99d67d918648d
DeLettenhove <deletten@gmail.com>
2020-11-11 17:00:36 +0200

    Before JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-$(document).ready(function(){
-  $('.slider').slick({
+$(document).ready(function () {
+  $(".slider").slick({
+    dots: true,
   });
+});
+
+let humburger = document.getElementsByClassName(".product__button");
+let menu = document.getElementsByClassName(".menu");
+
+humburger.addEventListener("click", function () {
+  menu.style.display = "block";
 });

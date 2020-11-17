8acec3da67d67f4535ad7ae61d46595156f24e3c
DeLettenhove <deletten@gmail.com>
2020-11-17 13:22:24 +0200

    Menu opening is ready

--- js/main.js
+++ js/main.js
@@ -5,14 +5,26 @@ $(document).ready(function () {
     infinite: false,
   });
 
-  $('.product__button_l').click(function(event) {
-    $(".slider").slick('goTo', 0);
+  $(".product__button_l").click(function (event) {
+    $(".slider").slick("goTo", 0);
   });
-  $('.product__button_m').click(function(event) {
-    $(".slider").slick('goTo', 1);
+  $(".product__button_m").click(function (event) {
+    $(".slider").slick("goTo", 1);
   });
-  $('.product__button_s').click(function(event) {
-    $(".slider").slick('goTo', 2);
+  $(".product__button_s").click(function (event) {
+    $(".slider").slick("goTo", 2);
   });
 });
 
+let welcome = document.querySelector(".welcome");
+let menu = document.querySelector(".menu");
+let menuBtnOpen = document.querySelector(".menu-btn__open");
+let menuBtnClose = document.querySelector(".menu-btn__close");
+
+function menuOpen() {
+  menu.classList.toggle('hidden');
+  welcome.classList.toggle('hidden');
+}
+
+menuBtnOpen.addEventListener("click", menuOpen);
+menuBtnClose.addEventListener("click", menuOpen);
